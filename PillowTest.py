80892614970e67ace7beb1ff6b0a7fafbbc4a5b1
meyerhallot <meyerwasup@gmail.com>
2017-01-17 13:31:11 -0500

    added obomify and test image

--- PillowTest.py
+++ PillowTest.py
@@ -1,6 +1,7 @@
 from PIL import Image
 class PillowTest:
     uslessVar = 0
+    thresh1 = 0 
     def __init__(self):
         uselessVar=0
 
@@ -10,5 +11,40 @@ class PillowTest:
         box = (100,100,400,400)
         region = im.crop(box)
         region.show()
+
+    """ Rios, Vickii"""
+    def obomify(self,path):
+	im= Image.open(path)
+	im.show()
+	pixels= list(im.getdata())
+
+	darkBlue = (0, 51, 76)
+	red = (217, 26, 33)
+	lightBlue = (112, 150, 158)
+	yellow = (252, 227, 166)
+
+	list_2= []
+
+	num = 0
+
+	for trip in pixels:
+	    b=sum(trip)
+	    #print (b)
+
+	    if b < self.thresh1:
+	        list_2.append(darkBlue)
+	    elif b > 182 and b < 364:
+	        list_2.append(red)
+	    elif b > 364 and b < 546:
+	        list_2.append(lightBlue)
+	    else:
+		list_2.append(yellow)
+
+
+	newpic = Image.new("RGB" , (im.size))
+	newpic.putdata(list_2)
+
+	newpic.show()
+
 
 
--- otherPillowTest.py
+++ otherPillowTest.py
@@ -1,3 +1,4 @@
 from PillowTest import PillowTest
 thing = PillowTest()
-thing.testFunc("/home/meyerhallot/Pictures/Screenshot from 2017-01-07 10-58-00.png")
+thing.obomify("/home/meyerhallot/Pictures/Screenshot from 2017-01-07 10-58-00.png")
+thing.thresh1 = 180
