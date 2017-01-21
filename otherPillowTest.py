758eb438e036155598c68e0f3c924584795b5f07
meyerhallot <meyerwasup@gmail.com>
2017-01-20 19:02:24 -0500

    removed old files, fixed linux file path

--- otherPillowTest.py
+++
@@ -1,7 +0,0 @@
-from PillowTest import PillowTest
-thing = PillowTest()
-#thing.testFunc("r.png")
-#thing.obomify("r.png")
-#thing.blurImage("r.png")
-thing.contourImage("r.png")
-thing.thresh1 = 180
